f6f039e46c0d7e4889d7d170f344a45adfadf0ac
Vaibhav Choudhary <chanakya-vc@users.noreply.github.com>
2016-03-18 03:02:55 +0530

    Create Hellodrivertry1.c

---
+++ Drivers/Hellodrivertry1.c
@@ -0,0 +1,12 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+int init_module(void)
+{
+	printk(KERN_ALERT "Hello Humans,This is VC speaking");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_ALERT "Goodbye Humans");
+}
