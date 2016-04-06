e7cb6e0a58fd183d6de405f2948f9244b141ce8c
Vaibhav Choudhary <chanakya-vc@users.noreply.github.com>
2016-04-06 20:14:23 +0530

    Update query_ioctls.h

--- ioctlsdriver/query_ioctls.h
+++ ioctlsdriver/query_ioctls.h
@@ -1,3 +1,4 @@
+//QUERY_IOCTL.h
 #ifndef QUERY_IOCTL_H
 #define QUERY_IOCTL_H
 #include <linux/ioctl.h> //For including <asm/ioctl.h to help with choosing ioctl numbers>
@@ -8,3 +9,4 @@
 #define query_getvar  _IOR('q',1,query_for_ioctls *);
 #define query_clearvar  _IO('q',2);
 #define query_setvar  _IOW('q',3,query_for_ioctls *);
+#endif
